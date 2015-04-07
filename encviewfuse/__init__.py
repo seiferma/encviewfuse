f246c7ef3e6a430b70a65803d6feb5650893c958
Stephan Seifermann <seiferma@users.noreply.github.com>
2015-04-07 19:36:07 +0200

    Moved tests.

--- encviewfuse/__init__.py
+++ encviewfuse/__init__.py
@@ -1,5 +1,5 @@
-# from pkgutil import extend_path
-# __path__ = extend_path(__path__, __name__)
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
 
 from pkg_resources import declare_namespace
 declare_namespace('encviewfuse')
